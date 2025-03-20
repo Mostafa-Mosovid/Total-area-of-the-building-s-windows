b8f231c3cd0364245118836c845ec509ad2224f4
Mostafa-Mosovid <mostafa.goodarzi1997@gmail.com>
2025-03-20 15:46:29 +0330

    Add files via upload

---
+++ poroje.py
@@ -0,0 +1,9 @@
+import csv 
+total_area = 0.0
+with open('D:\Python\jadi\github\windows_dimensions.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        length = float(row['length'])
+        width = float(row['width'])
+        total_area += length * width
+print(f"Total area of windows is: {total_area}")
